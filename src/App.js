458935388539b7dac707031de2d1bacca15c9e7f
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2024-01-21 04:08:39 +0600

    UI Design Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <AllRoutes />
     </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,32 @@
+import { useTitle } from "../hooks/useTitle";
+import { CartCard } from "../components";
+
 export const Cart = () => {
+  useTitle("Cart");
+
+  const products = [
+    {
+      id: 1,
+      name: "Sony Wh-Ch510 Bluetooth Wireless",
+      price: 149,
+      image: "/assets/images/001.jpg",
+    },
+    {
+      id: 2,
+      name: "boAt Rockerz 450",
+      price: 49,
+      image: "/assets/images/002.jpg",
+    },
+  ];
+
   return (
-    <div>Cart</div>
-  )
-}
+    <main>
+      <section className="cart">
+        <h1>Cart Items: {products.length}</h1>
+        {products.map((product) => (
+          <CartCard key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  );
+};
